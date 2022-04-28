ed95de6ca2f91f62bc87d10823c53765f9cb66fd
meowjesty <meowjesty_nyan@outlook.com>
2022-04-27 22:29:38 -0300

    Remove unneeded dependencies from package.json.

--- xt/src/extension.ts
+++ xt/src/extension.ts
@@ -12,7 +12,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import fetch from "node-fetch";
 
 // NOTE(alex): Made it work!
 //
